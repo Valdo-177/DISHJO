af434c65e26e2ff8eae76745dd98b35cdb7512a7
Valdo-117 <keyerdelahozsteam@gmail.com>
2024-12-01 23:21:37 -0500

    animated 2.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import HomePages from "@/pages/HomePages";
+import dynamic from 'next/dynamic'
+
+const DynamicHome = dynamic(() => import("@/pages/HomePages"), {
+  loading: () => <p>Loading...</p>,
+})
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <HomePages/>
+      <DynamicHome />
     </div>
   );
 }
