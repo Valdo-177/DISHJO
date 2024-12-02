ad6cac475038e3d70d4778b998cbd1609c775e87
Valdo-117 <keyerdelahozsteam@gmail.com>
2024-12-01 23:37:08 -0500

    ssr: false

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic'
 
 const DynamicHome = dynamic(() => import("@/pages/HomePages"), {
+  ssr: false,
   loading: () => <p>Loading...</p>,
 })
 
--- src/components/ui/myAnimation.tsx
+++ src/components/ui/myAnimation.tsx
@@ -1,14 +1,24 @@
 import Lottie from "lottie-react";
 import myAnimation from "@/assets/data_Text.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const MyAnimationComponent = ({ children }: { children: React.ReactElement }) => {
     const [show, setShow] = useState(true);
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true); // Hacer que el código que depende del cliente se ejecute después de la carga
+    }, []);
+
+    if (!isClient) {
+        return null; // No renderizar nada en el servidor
+    }
 
     setTimeout(() => {
         setShow(false)
     }, 2500);
+    
     return (
         <div>
             {show && <Lottie
@@ -17,13 +27,13 @@ const MyAnimationComponent = ({ children }: { children: React.ReactElement }) =>
                 style={{ width: "300px", height: "300px" }}
             />}
             {!show && <motion.div
-                initial={{ opacity: 0, y: 50 }} 
-                animate={{ opacity: 1, y: 0 }} 
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
                 transition={{
-                    duration: 0.3, 
-                    ease: "easeOut", 
+                    duration: 0.3,
+                    ease: "easeOut",
                 }}
-              
+
             >
                 {children}
             </motion.div>}
